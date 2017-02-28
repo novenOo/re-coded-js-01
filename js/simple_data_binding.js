4a49ff49c41711227c9fb3e92d5e9f40b8b8edcd
Noven Temer <noven5823@gmail.com>
2017-02-28 19:42:59 +0300

    re-coded-js

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  
+ 
 });
+var input= $('#textArea'),
+    mydiv= $('#myDiv');
+    input.keyup(function(e){ /* update a myDiv in the page with the text the user enter in a textArea*/
+    	mydiv.text(input.val());
+    })
