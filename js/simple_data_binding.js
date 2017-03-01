5fa961323e010f86c80f5c528ac72a0c9c6e7fcb
Noven Temer <noven5823@gmail.com>
2017-03-01 13:57:08 +0300

    re-coded

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
- 
-});
-var input= $('#textArea'),
+ var input= $('#textArea'),
     mydiv= $('#myDiv');
     input.keyup(function(e){ /* update a myDiv in the page with the text the user enter in a textArea*/
     	mydiv.text(input.val());
     })
+});
